34136b18ccb3aa571be291af2f67fd674d87050b
Vanderson10 <araujovanderson223@gmail.com>
2025-08-19 19:37:49 -0300

    ajuste bd

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,8 +1,8 @@
 import pymongo
 from datetime import datetime
 
-client = pymongo.MongoClient("mongodb://10.158.0.3:27017/")
- 
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+
 db = client["coralinia"]
 
 chats_collection = db["chats"]
@@ -17,11 +17,12 @@ def save_message(key: str, text: str, message_type: str = "user"):
         "timestamp": datetime.now(),
     }
     chats_collection.insert_one(message_data)
-    
+
 
 def get_messages_by_key(key: str):
     """Recupera as mensagens do MongoDB com base na key."""
-    messages = chats_collection.find({"key": key}).to_list(length=None) # ordenar por timestamp
+    messages = chats_collection.find({"key": key}).to_list(
+        length=None)  # ordenar por timestamp
     return messages
 
 
